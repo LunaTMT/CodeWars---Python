ab90eb3d2d816157f5dc5480ea65e0509021c754
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-11-22 11:39:17 +0000

    Add files via upload
    not optimised with backtracking - uncompleted kata

---
+++ Square_into_Squares.py
@@ -0,0 +1,28 @@
+import math
+
+def check_increasing(array):
+    if len(array) > len(set(array)):
+        return None
+    return array
+
+def decompose(n):
+    answer = [n-1]
+    quotient = n**2 - (n-1)**2
+
+    #Dividend / Divisor  = Quotient with Remainder
+
+    while quotient >= 1:
+        sqr_rt = int(math.sqrt(quotient))
+        answer.append(sqr_rt)
+        quotient = quotient - sqr_rt**2 
+    
+    if quotient == 1:
+        answer.append(1)
+
+    return check_increasing(answer[::-1])
+
+if __name__ == "__main__":
+    #decompose(30)
+    #decompose(5)    #, [3,4])
+    decompose(2256307)
+    decompose(8)    #, None)
