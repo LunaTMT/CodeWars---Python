086792a074efd9302340cda266f4349dd6ddc4d9
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-11-30 11:33:45 +0000

    Add files via upload
    completed kata but does not meet the requirement for efficiency.

---
+++ how_many_numbers_III.py
@@ -0,0 +1,36 @@
+
+"""
+def find_all(sum_dig, digs):
+    valid = []
+
+    for i in range(int("9" * (digs-1)) + 1, int("9"*digs) + 1):
+        split = [int(x) for x in str(i)]
+        
+        if sum(split) == sum_dig and sorted(split) == split:
+                valid.append(i)
+
+    if not valid:
+        return []
+    return [len(valid), valid[0], valid[-1]]
+"""
+
+from itertools import combinations_with_replacement
+
+def find_all(sum_dig, digs):
+    combs = combinations_with_replacement(list(range(1, 10)), digs)
+    target = [''.join(str (x) for x in list(comb)) for comb in combs if sum(comb) == sum_dig]
+    if not target:
+        return []
+    return [len(target), int(target[0]), int(target[-1])]
+
+
+if __name__ == "__main__":
+    find_all(10, 3) #, [8, 118, 334])
+    find_all(27, 3) #, [1, 999, 999])
+    find_all(84, 4) #, [])
+    find_all(35, 6) #, [123, 116999, 566666])
+
+
+    from itertools import combinations_with_replacement
+
+
