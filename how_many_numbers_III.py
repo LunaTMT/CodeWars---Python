15bd40385d7a1ba7e7aa357dd14cf358882353df
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-11-30 11:36:41 +0000

    Update how_many_numbers_III.py

--- how_many_numbers_III.py
+++ how_many_numbers_III.py
@@ -1,5 +1,7 @@
+# completed kata but does not meet the requirement for efficiency.
+# https://www.codewars.com/kata/5877e7d568909e5ff90017e6/train/python
+# Kata Author: raulbc777
 
-"""
 def find_all(sum_dig, digs):
     valid = []
 
@@ -12,16 +14,7 @@ def find_all(sum_dig, digs):
     if not valid:
         return []
     return [len(valid), valid[0], valid[-1]]
-"""
 
-from itertools import combinations_with_replacement
-
-def find_all(sum_dig, digs):
-    combs = combinations_with_replacement(list(range(1, 10)), digs)
-    target = [''.join(str (x) for x in list(comb)) for comb in combs if sum(comb) == sum_dig]
-    if not target:
-        return []
-    return [len(target), int(target[0]), int(target[-1])]
 
 
 if __name__ == "__main__":
@@ -31,6 +24,17 @@ if __name__ == "__main__":
     find_all(35, 6) #, [123, 116999, 566666])
 
 
-    from itertools import combinations_with_replacement
 
+    
+"""
+#TOP
 
+from itertools import combinations_with_replacement
+
+def find_all(sum_dig, digs):
+    combs = combinations_with_replacement(list(range(1, 10)), digs)
+    target = [''.join(str (x) for x in list(comb)) for comb in combs if sum(comb) == sum_dig]
+    if not target:
+        return []
+    return [len(target), int(target[0]), int(target[-1])]
+"""
