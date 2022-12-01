a0f55dd36e41b7b12e83dae724b7648be604c77b
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-01 13:35:05 +0000

    Update Middle_Permutation.py

--- Middle_Permutation.py
+++ Middle_Permutation.py
@@ -1,23 +1,21 @@
-#from itertools import permutations
+# https://www.codewars.com/kata/58ad317d1541651a740000c5/solutions
+# Could not finish due to optimisation constrain on kata
+# Kata Author: myjinxin2015 
+# 4 kyu
+
 import itertools
 
 def middle_permutation(string):
-
-
     permutations = list(itertools.permutations(sorted(string)))
- 
-    # Output: ['ABC', 'ACB', 'BAC', 'BCA', 'CAB', 'CBA']
     permutations = ([''.join(permutation) for permutation in permutations])
     middleIndex = (len(permutations) - 1)/2
     
-
-    print(permutations[int(middleIndex)])
     return permutations[int(middleIndex)]
 
 if __name__ == "__main__":
-        #middle_permutation("abc")       #,"bac")
-        #middle_permutation("abcd")      #,"bdca")
-        #middle_permutation("abcdx")     #,"cbxda")
-        #middle_permutation("abcdxg")    #,"cxgdba")
-        #middle_permutation("abcdxgz")   #,"dczxgba")
-        middle_permutation("gybpitcfsanwohjvdqlxue")  
+        middle_permutation("abc")       #,"bac")
+        middle_permutation("abcd")      #,"bdca")
+        middle_permutation("abcdx")     #,"cbxda")
+        middle_permutation("abcdxg")    #,"cxgdba")
+        middle_permutation("abcdxgz")   #,"dczxgba")
+        middle_permutation("gybpitcfsanwohjvdqlxue")  #Not optimised to handle <---
