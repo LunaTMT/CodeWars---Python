6ca8aca1780d2df4fdfac49b4b5ccb3222eede75
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-05 19:08:03 +0000

    Update Moving_Zeros_To_The_End.py

--- Moving_Zeros_To_The_End.py
+++ Moving_Zeros_To_The_End.py
@@ -1,7 +1,8 @@
-def move_zeros(arr):
+# https://www.codewars.com/kata/52597aa56021e91c93000cb0
+# kata author: xcthulhu
+# 5kyu
 
-    l = [i for i in arr if i!=0]    
-    return l+[0]*(len(arr)-len(l)) 
+def move_zeros(lst):
 
     for i in range(0, len(lst)):
         if lst[i] == 0:
@@ -10,6 +11,10 @@ def move_zeros(arr):
             lst.insert(len(lst), item)
     return lst
 
-
 if __name__ == "__main__":
     move_zeros([1, 0, 1, 2, 0, 1, 3])
+    
+    
+    
+#   l = [i for i in arr if i!=0]   
+#   return l+[0]*(len(arr)-len(l))    
