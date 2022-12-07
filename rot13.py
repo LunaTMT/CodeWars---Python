2910c990df850d4a05767ba84bb58df0d5806726
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-07 15:38:25 +0000

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -1,32 +1,13 @@
+# https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+# Kata author: Rubikan
+# 5 kyu
+
 def rot13(message):
-
-
-    alphabet = list(map(chr, range(97, 123)))
-    shifted = list(map(chr, range(110, 123))) + list(map(chr, range(97, 110)))
-    dict_ = {alphabet[i]: shifted[i] for i in range(len(alphabet))}
-
-
-    cap = [] #Boolean list - Determines capital letter index
-    
-    for i in range(0, len(message)):
-        if message[i].isupper():
-            cap.append(True)
-        cap.append(False)
-    
-    
-    letters = list(message.lower())
-    output = [dict_[char] for char in letters]
-
-
-    for i in range(0, len(cap)):
-        if cap[i]:
-            output[i] = output[i].upper()
-    return output
+    alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.!@#$%^&*+?~()_-+=:;<>'
+    shifted = ' NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm1234567890.!@#$%^&*+?~()_-+=:;<>'
+    dict_ = {[*alphabet][i]: [*shifted][i] for i in range(len(alphabet))}
+    return "".join([dict_[char] for char in list(message)])
 
 if __name__ == "__main__":
-   # rot13('test')
-    rot13('Test')
-    #rot13('aA bB zZ 1234 *!?%')
-
-
-
+    rot13("Test")
+    rot13('aA bB zZ 1234 *!?%')
