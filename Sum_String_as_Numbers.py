b403261bdad1b7b9fba8275c61da6b60b24894a0
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-07 14:09:22 +0000

    Update Sum_String_as_Numbers.py

--- Sum_String_as_Numbers.py
+++ Sum_String_as_Numbers.py
@@ -1,30 +1,22 @@
-def printXY(x, y):
-    print("X: ", x)
-    print("Y: ", y)
-
+# https://www.codewars.com/kata/5324945e2ece5e1f32000370/solutions
+# Kata Author: nicknovitski
+# 4 Kyu
+# Not completed: Does not work for float(inf) values - exe timeout
 
 def sum_strings(x, y):
-    print()
-    
+
     if x and y:
-        print("C1")
-        printXY(x, y)
-        return str(int(float(x)) + int(float(y)))
-    
-    elif not x and not y:        
-        print("C2")
-        printXY(x, y)
-        return '0'
-    
-    elif not x and y:       
-        print("C3")
-        printXY(x, y)
-        return y
-    
+        return str(int(x) + int(y))
     elif x and not y:
-        print("C4")
-        printXY(x, y)
         return x
+    elif not x and y:       
+        return y
+    elif not x and not y:        
+        return '0'      
+    else:
+        return str(int(float(x)) + int(float(y)))
+
 
 if __name__ == "__main__":
-    sum_strings(9999999999999999, 1)
+    sum_strings("1", "1")       #, "2")
+    sum_strings("123", "456")   #, "579")
