bc07e1b61986bdfcc61812142494df9b3941b25f
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-22 12:29:10 +0000

    Update weight.py

--- weight.py
+++ weight.py
@@ -3,13 +3,11 @@
 # 5 kyu
 
 def order_weight(string):
-
     #spliting string into elements
     # "103 123 4444 99 2000" -> ['2000', '10003', '1234000', '44444444', '9999', '11', '11', '22', '123']
     values = string.split(' ') 
     
     #splitting each value in the string into its own list and summing it
-
     weights =  [sum([int(x) for x in value]) for value in values] 
 
     #sorting by weights 
