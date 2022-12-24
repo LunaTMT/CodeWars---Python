50da5a19818085d0cd17c8750c2bcfb298b42a11
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 16:29:34 +0000

    Update descending_order.py

--- descending_order.py
+++ descending_order.py
@@ -1,7 +1,5 @@
 def descending_order(num):
-    arr = list(map(str, str(num)))
-    arr.sort(reverse=True)
-    return int("".join(arr))
+    return int("".join(sorted(str(num), reverse=True)))
 
 if __name__ == "__main__":
     descending_order(123456789)
