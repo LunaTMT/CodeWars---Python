6a63130c6ec9c159f22a4034ec4b83fa36109da0
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 15:59:05 +0000

    Update array_diff.py

--- array_diff.py
+++ array_diff.py
@@ -1,11 +1,9 @@
+# https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+# Kata Author: marcinbunsch
+# 6 kyu
 
 def array_diff(a, b):
-
-    new_list =  list(filter(lambda x: (x not in b), a))
-
-
-    print(list(filter(bool, new_list)))
-    return list(filter(bool, new_list))
+    return list(filter(lambda x: (x not in b), a))
 
 if __name__ == "__main__":
     array_diff([1, 2, 2], [2])
