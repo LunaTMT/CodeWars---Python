0c5ddec0d74815d488eee66f0dac969ef7a99373
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 13:21:44 +0000

    Update simple_str_manip.py

--- simple_str_manip.py
+++ simple_str_manip.py
@@ -1,3 +1,7 @@
+# Kata: https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+# Katau Author: xDranik
+# 6kyu
+
 def spin_words(sentence):
     words = sentence.split(' ')
     words = list(map(lambda x: x[::-1] if len(x) >= 5 else x, words))
