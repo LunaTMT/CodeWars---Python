2d4288f80ae89f0438a62e7da7a824767c5bc125
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 15:24:11 +0000

    Update sum_of_digits.py

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,19 +1,19 @@
 # https://www.codewars.com/kata/541c8630095125aba6000c00/train/python
 # Kata Author: user578387
 # 6 kyu
+
 def recusive_func(value):
     value = sum(value)
     if len(str(value)) >= 2:
         return recusive_func(list(map(int, str(value))))
-    return False, value
+    return value
 
 
 def digital_root(n):
-    _ = True
-    while _:
-        _, n = recusive_func(list(map(int, str(n))))
-    print()
+    while len(str(n)) > 1:
+        n = recusive_func(list(map(int, str(n))))
     return n
+    
     
 
 if __name__ == "__main__":
