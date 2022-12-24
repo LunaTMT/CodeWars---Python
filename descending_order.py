01f75676c3441aad2b6febe8eeaf824de3382293
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 16:30:21 +0000

    Update descending_order.py

--- descending_order.py
+++ descending_order.py
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/python
+# Kata Author: TastyOs
+# 7 Kyu
+
 def descending_order(num):
     return int("".join(sorted(str(num), reverse=True)))
 
