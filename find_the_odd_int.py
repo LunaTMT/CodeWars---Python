9968330f27b757e13e21746862994b7161dc756f
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-24 13:41:15 +0000

    Update find_the_odd_int.py

--- find_the_odd_int.py
+++ find_the_odd_int.py
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+# Kata Author: rbuckley
+# 6 kyu
+
 def find_it(seq):
     answer = list(map(lambda x: x if seq.count(x) % 2 > 0 else None, set(seq)))
     value = [val for val in answer if val != None]
