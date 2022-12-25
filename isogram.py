c9398e54f433f569997267806e2f6c5779fa76de
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-25 10:44:28 +0000

    Update isogram.py

--- isogram.py
+++ isogram.py
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/python
+# Kata Author: chunjef
+# 7 kyu
+
 def is_isogram(string):
     string = string.lower()
     if len([True for char in set(string) if string.count(char) > 1]) >= 1:
