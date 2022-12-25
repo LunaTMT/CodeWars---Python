ca0e0e59b78f07f49c68762588d3c97a3642f67b
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-25 14:20:49 +0000

    Update counting_duplicates.py

--- counting_duplicates.py
+++ counting_duplicates.py
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/
+# Kata Author: kgashok
+# 6 Kyu
+
 def duplicate_count(text):
     check = list(map(lambda x: True if text.count(x) > 1 else False, [x for x in set(text.lower())]))
     print()
