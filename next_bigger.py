b1457b9ec631bb234849910a755a3b97133b59f7
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-28 15:54:14 +0000

    Update next_bigger.py

--- next_bigger.py
+++ next_bigger.py
@@ -1,3 +1,10 @@
+# https://www.codewars.com/kata/55983863da40caa2c900004e/train/python
+# Kata Author: GiacomoSorbi
+# 4 kyu
+
+#Unfinished kata, works but but not optimised.
+
+
 from itertools import permutations
 
 def next_bigger(n):
