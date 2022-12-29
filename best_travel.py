f7bfab89ce7ba67dae2161369f4fa6d2047f50ec
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2022-12-29 17:26:30 +0000

    Update best_travel.py

--- best_travel.py
+++ best_travel.py
@@ -1,18 +1,16 @@
+# https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa/train/python
+# Kata Author: g964
+# 5 kyu
+
 
 from itertools import combinations
 
 def choose_best_sum(t, k, ls):
-    valids = []
-
-    for value in list(combinations(ls, k)):
-        if(sum(value)) <= t:
-            valids.append(sum(value))
-
-    print(valids)
+    valids = [sum(value) for value in list(combinations(ls, k)) if sum(value) <= t]
+    return max(valids) if valids else None
 
 
 if __name__ == "__main__":
-
     xs = [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]
     choose_best_sum(230, 4, xs)    #, 230)
     choose_best_sum(430, 5, xs) #, 430)
