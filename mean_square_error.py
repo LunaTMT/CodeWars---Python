aa2f9bb8f56dbf6629c2036fd35be57b56cba212
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-01-01 13:31:47 +0000

    Update mean_square_error.py

--- mean_square_error.py
+++ mean_square_error.py
@@ -1,2 +1,6 @@
+# https://www.codewars.com/kata/51edd51599a189fe7f000015/python
+# Kata Author: kylehill
+# 5 kyu
+
 def solution(a, b):
     return sum([pow(abs(i - j), 2) for i, j in zip(a, b)]) / len(a)
