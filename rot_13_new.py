62cfcfe374ae1e193746d27ea768446823f9cb3c
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-01-03 17:06:29 +0000

    Add files via upload

---
+++ rot_13_new.py
@@ -0,0 +1,31 @@
+def get_chr(i):
+    unicode_val = ord(i)
+    rot_13 = unicode_val + 13
+
+    if unicode_val < 65 or (91 <= unicode_val <= 96) or unicode_val > 122:
+        return i
+
+    elif i == i.upper():
+        if rot_13 > 90:
+            return chr((rot_13 % 90) + 64)
+        else:
+            return chr(rot_13)
+
+    elif i == i.lower():
+        if rot_13 > 122:
+            return chr((rot_13 % 122) + 96)
+        else:
+            return chr(rot_13)
+    else:
+        return i
+
+
+def rot13(message):
+    return "".join([get_chr(i) for i in message])
+
+
+
+
+if __name__ == "__main__":
+    #rot13(" rknzcyr.") #, "ROT13 example.")
+    rot13("@[`{")   #, "@[`{")
