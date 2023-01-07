17c91065b2d34fc2ebb5e52eb2fed2886a72e79c
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-01-07 21:42:12 +0000

    Update replace_alphabet_position.py

--- replace_alphabet_position.py
+++ replace_alphabet_position.py
@@ -1,3 +1,7 @@
+# https://www.codewars.com/kata/546f922b54af40e1e90001da/solutions/python
+# Kata Author: MysteriousMagenta
+# 6 kyu
+
 def alphabet_position(text):
     alphabet = list(map(chr, range(97, 123)))
     dict_ = {letter: str(i+1) for i, letter in enumerate(alphabet)} 
