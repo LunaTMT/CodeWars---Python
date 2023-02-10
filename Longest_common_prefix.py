e5b9935b3cd657a5cd1bfab1dc96761153892fcb
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-02-10 17:14:52 +0000

    Add files via upload

---
+++ Longest_common_prefix.py
@@ -0,0 +1,39 @@
+
+
+
+def longestCommonPrefix(strs):
+
+
+        inter = set(strs[0])
+        for item in strs:
+            inter = inter & set(item)
+
+        
+        if not inter:
+            return ""
+
+        if len(strs) == 1:
+            return strs[0]
+        
+        valid = []
+
+        for letter in inter:
+            i = 1
+            indexs = [item.index(letter) for item in strs]
+
+            while True:
+                
+                slices = [item[indexs[idx] : indexs[idx] + i] for idx, item in enumerate(strs)]
+
+                if len((set(slices))) != 1:
+                    if i >= 2:
+                        valid.append(slices[0][:-1])
+                    break
+               
+                i += 1
+
+        return max(valid, key=len)
+
+if __name__ == "__main__":
+    strs = ["cir","car"]
+    longestCommonPrefix(strs)
