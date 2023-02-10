eea5dfe16046cf9eaf471ee611b2b021c0f40dd0
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-02-10 17:17:15 +0000

    Update Longest_common_substring.py

--- Longest_common_substring.py
+++ Longest_common_substring.py
@@ -1,14 +1,12 @@
 
 
 
-def longestCommonPrefix(strs):
-
-
+def longestCommonSS(strs):
+        
         inter = set(strs[0])
         for item in strs:
             inter = inter & set(item)
 
-        
         if not inter:
             return ""
 
@@ -22,18 +20,15 @@ def longestCommonPrefix(strs):
             indexs = [item.index(letter) for item in strs]
 
             while True:
-                
                 slices = [item[indexs[idx] : indexs[idx] + i] for idx, item in enumerate(strs)]
 
                 if len((set(slices))) != 1 or i > max(strs, key=len):
                     if i >= 2:
                         valid.append(slices[0][:-1])
                     break
-               
                 i += 1
-
         return max(valid, key=len)
 
 if __name__ == "__main__":
-    strs = ["cir","car"]
+    strs = ["reflower","flow","flight"]
     longestCommonPrefix(strs)
