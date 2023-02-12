3248227b8aed5e8dec810b38e58275b9c187b7b0
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-02-12 16:14:58 +0000

    Update How_many_are_smaller_than_me.py

--- How_many_are_smaller_than_me.py
+++ How_many_are_smaller_than_me.py
@@ -1,2 +1,7 @@
+# https://www.codewars.com/kata/56a1c074f87bc2201200002e/train/python
+# joh_pot 
+# 7 kyu
+
+
 def smaller(arr):
     return [sum([1 for i in arr[idx:] if i < value]) for idx, value in enumerate(arr)]
