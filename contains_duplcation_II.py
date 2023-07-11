a5dfb0f422aabfcbfc3784d1d18b30805dc15747
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-07-11 19:39:06 +0100

    Delete contains_duplcation_II.py

--- contains_duplcation_II.py
+++
@@ -1,9 +0,0 @@
-def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-    
-    found = {}
-
-    for i, num in enumerate(nums):
-        if num in found and abs(found[num] - i) <= k:
-            return True
-        found[num] = i
-    return False
